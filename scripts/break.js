22e7e183f719357cf9ba639344636d8a3acaabd8
Auberon López <auberon@adadev.org>
2021-11-13 10:57:26 -0800

    Use block js comments and wrap to 80 columns.

--- scripts/break.js
+++ scripts/break.js
@@ -7,8 +7,10 @@ MouseConstraint = Matter.MouseConstraint;
 
 let simulationStarted = false;
 
-// Returns the offset of an element relative to the document.
-// Taken from https://plainjs.com/javascript/styles/get-the-position-of-an-element-relative-to-the-document-24/
+/**
+ * Returns the offset of an element relative to the document. Taken from
+ * https://plainjs.com/javascript/styles/get-the-position-of-an-element-relative-to-the-document-24/
+ */
 function offset(el) {
     const rect = el.getBoundingClientRect(),
     scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
@@ -16,24 +18,30 @@ function offset(el) {
     return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
 }
 
-// Returns the location of a rectangle's top left corner given
-// the centroid. Assumes rectangle is parallel to x axis.
+/** 
+ * Returns the location of a rectangle's top left corner given
+ * the centroid. Assumes rectangle is parallel to x axis.
+ */
 function centroidToTopLeft(x, y, width, height) {
     return {left: Math.floor(x - (width/2)),
             top: Math.floor(y - (height/2))}
 }
 
-// Returns the location of a rectangle's centroid given
-// the coordinates of the top left corner. Assumes rectangle 
-// is parallel to x axis.
+/** 
+ * Returns the location of a rectangle's centroid given
+ * the coordinates of the top left corner. Assumes rectangle 
+ * is parallel to x axis.
+ */
 function topLeftToCentroid(left, top, width, height) {
     return {x: Math.floor(left + (width/2)),
             y: Math.floor(top + (height/2))}
 }
 
-// Returns a rectangle Matterjs body with the same location and
-// dimensions as the DOM element passed in. Stores a reference
-// to the DOM element in render.element.
+/**  
+ * Returns a rectangle Matterjs body with the same location and dimensions the
+ * DOM element passed in. Stores a reference to the DOM element in
+ * render.element.
+ */
 function rectangleFromElement(element, isStatic) {
     const location = offset(element)
 
@@ -41,9 +49,8 @@ function rectangleFromElement(element, isStatic) {
     const elemWidth = parseFloat(style.width);
     const elemHeight = parseFloat(style.height);
 
-    // Translate position from CSS to Matterjs.
-    // CSS tracks elements by their top-left corner,
-    // Matterjs tracks them by their centroid.
+    // Translate position from CSS to Matterjs. CSS tracks elements by their
+    // top-left corner, Matterjs tracks them by their centroid.
     const bodyPos = topLeftToCentroid(location.left, location.top, 
                                       elemWidth, elemHeight);
 
@@ -59,10 +66,12 @@ function rectangleFromElement(element, isStatic) {
                                   });
 }
 
-// Creates a rectangle body for each DOM element of class "dynamic"
-// or "static". Each body will be sized and positioned to fit the element.
-// Dynamic bodies will have isStatic set to false, static bodies will have it
-// set to true. Returns an array of the bodies.
+/** 
+ * Creates a rectangle body for each DOM element of class "dynamic" or "static".
+ * Each body will be sized and positioned to fit the element. Dynamic bodies
+ * will have isStatic set to false, static bodies will have it set to true.
+ * Returns an array of the bodies.
+ */
 function bodiesFromDocument() {
     const bodies = [];
 
@@ -78,8 +87,10 @@ function bodiesFromDocument() {
     return bodies;
 }
 
-// Takes matterjs body and updates its corresponding HTML element
-// with styles corresponding to the body's location and rotation.
+/** 
+ * Takes matterjs body and updates its corresponding HTML element with styles
+ * corresponding to the body's location and rotation.
+ */
 function updateBodyElement(body) {
     // If the body has no element, there's nothing to update.
     if (body.render == null || body.render.element == null) {
@@ -98,11 +109,13 @@ function updateBodyElement(body) {
 }
 
 
-// Begins running simulation and rendering.
-// Creates rectangular physics bodies for each DOM element of class "dynamic"
-// or "static". Runs a physics simulation and updates CSS of the DOM elements
-// to match translation and rotation of the physics objects.
-// By default each physics step assumes 16 ms has passed (~60 fps)
+/** 
+ * Begins running simulation and rendering.
+ * Creates rectangular physics bodies for each DOM element of class "dynamic"
+ * or "static". Runs a physics simulation and updates CSS of the DOM elements
+ * to match translation and rotation of the physics objects.
+ * By default each physics step assumes 16 ms has passed (~60 fps)
+ */
 function startSimulation(update_ms=16) {
     // Only start the simulation once
     if (simulationStarted) {
@@ -117,9 +130,8 @@ function startSimulation(update_ms=16) {
 
     World.add(engine.world, bodiesFromDocument());
 
-    // Runs a step of the simulation at each animation frame.
-    // Updates the engine and the CSS of all DOM elements tied to
-    // a physics body.
+    /* Runs a step of the simulation at each animation frame. Updates the engine
+     and the CSS of all DOM elements tied to a physics body. */
     function simulationStep() {
         Engine.update(engine, update_ms);
 
